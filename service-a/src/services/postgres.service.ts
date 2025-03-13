8c8fa8e15bc3511a54f746cca554f108436ebcd3
Subodh Adhikari <subodh1adhikari@gmail.com>
2025-03-14 03:17:29 +0545

    feat: Create service-a that subscribe to kafa
    Models are copied between services(from gate-way api to service-a). This should be fixed later using a monorepo tool .

---
+++ service-a/src/services/postgres.service.ts
@@ -0,0 +1,29 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER || "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  database: process.env.POSTGRES_DB || "microservices",
+  password: process.env.POSTGRES_PASSWORD || "password",
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+});
+
+export const insertMessage = async (
+  userId: string,
+  message: string,
+  serviceKey: string,
+  randomKey: string
+) => {
+  try {
+    await pool.query(
+      "INSERT INTO messages (user_id, message, service_key, random_key) VALUES ($1, $2, $3, $4)",
+      [userId, message, serviceKey, randomKey]
+    );
+    console.log(`Message inserted for ${serviceKey}.`);
+  } catch (error) {
+    console.error(`Error inserting message for ${serviceKey}:`, error);
+  }
+};
